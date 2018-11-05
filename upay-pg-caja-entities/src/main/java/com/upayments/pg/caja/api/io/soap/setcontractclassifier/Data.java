3759c82b8a99cc9f1d15b51ab1e6a8ca3e03c027
Tomas Thoms Huerta <tthoms@gmail.com>
2018-11-04 23:31:34 -0300

    Servicio Seteo Clasificador de Contrato

---
+++ upay-pg-caja-entities/src/main/java/com/upayments/pg/caja/api/io/soap/setcontractclassifier/Data.java
@@ -0,0 +1,135 @@
+package com.upayments.pg.caja.api.io.soap.setcontractclassifier;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonPropertyOrder({ "BankCode", "ContractIdentifier", "Classifier",
+	"Value", "DateFrom", "DateTo", "Reason", "ApplRegNumber", "UserInfo"})
+public class Data implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	/*
+    <req:BankCode>?</req:BankCode>
+    <req:ContractIdentifier>?</req:ContractIdentifier>
+    <req:Classifier>?</req:Classifier>
+    <!--Optional:-->
+    <req:Value>?</req:Value>
+    <!--Optional:-->
+    <req:DateFrom>?</req:DateFrom>
+    <!--Optional:-->
+    <req:DateTo>?</req:DateTo>
+    <!--Optional:-->
+    <req:Reason>?</req:Reason>
+    <req:ApplRegNumber>?</req:ApplRegNumber>
+    <req:UserInfo>?</req:UserInfo>
+ </req:DATA>
+    */
+	
+	private String bankCode;
+	private String contractIdentifier; //	
+	private String classifier;
+	
+	private String value;
+	private String dateFrom;
+	private String dateTo;
+	private String reason;
+	
+	private String applRegNumber;
+	
+	private String userInfo; //		
+	
+	public Data() {
+		super();
+	}
+	
+	@JsonProperty("BankCode")
+	public String getBankCode() {
+		return bankCode;
+	}
+
+	public void setBankCode(String bankCode) {
+		this.bankCode = bankCode;
+	}
+
+	@JsonProperty("ContractIdentifier")
+	public String getContractIdentifier() {
+		return contractIdentifier;
+	}
+
+	public void setContractIdentifier(String contractIdentifier) {
+		this.contractIdentifier = contractIdentifier;
+	}
+
+	@JsonProperty("UserInfo")
+	public String getUserInfo() {
+		return userInfo;
+	}
+
+	public void setUserInfo(String userInfo) {
+		this.userInfo = userInfo;
+	}
+	
+	@JsonProperty("Classifier")
+	public String getClassifier() {
+		return classifier;
+	}
+
+	public void setClassifier(String classifier) {
+		this.classifier = classifier;
+	}
+
+	@JsonProperty("Value")
+	@JsonInclude(Include.NON_DEFAULT)		
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@JsonProperty("DateFrom")
+	@JsonInclude(Include.NON_DEFAULT)			
+	public String getDateFrom() {
+		return dateFrom;
+	}
+
+	public void setDateFrom(String dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+
+	@JsonProperty("DateTo")
+	@JsonInclude(Include.NON_DEFAULT)				
+	public String getDateTo() {
+		return dateTo;
+	}
+
+	public void setDateTo(String dateTo) {
+		this.dateTo = dateTo;
+	}
+
+	@JsonProperty("Reason")
+	@JsonInclude(Include.NON_DEFAULT)			
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	@JsonProperty("ApplRegNumber")
+	public String getApplRegNumber() {
+		return applRegNumber;
+	}
+
+	public void setApplRegNumber(String applRegNumber) {
+		this.applRegNumber = applRegNumber;
+	}
+
+}
