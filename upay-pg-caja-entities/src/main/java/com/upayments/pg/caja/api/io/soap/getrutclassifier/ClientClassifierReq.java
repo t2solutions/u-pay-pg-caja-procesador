c3a3b8654f2ba3935bc9b456e5526cc8ce2e251c
Tomas Thoms Huerta <tthoms@gmail.com>
2018-11-16 11:41:14 -0300

    Correccion problema POJO RutClassifier

--- upay-pg-caja-entities/src/main/java/com/upayments/pg/caja/api/io/soap/getrutclassifier/ClientClassifierReq.java
+++ upay-pg-caja-entities/src/main/java/com/upayments/pg/caja/api/io/soap/getrutclassifier/ClientClassifierReq.java
@@ -3,9 +3,8 @@ package com.upayments.pg.caja.api.io.soap.getrutclassifier;
 import java.io.Serializable;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.upayments.pg.caja.api.io.soap.getcontractclassifier.Data;
 
-public class ClientClassifierReq  implements Serializable {
+public class ClientClassifierReq implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
