215d2b92ed97a19852807953e873851f02343837
Tomas Thoms Huerta <tthoms@gmail.com>
2018-11-21 00:05:34 -0300

    Servicio Consulta de Offset de tarjeta por PAN

---
+++ u-pay-pg-ws-api-procesador/src/main/java/com/upayment/camel/route/GetCardPinOffsetByPanRoute.java
@@ -0,0 +1,68 @@
+package com.upayment.camel.route;
+
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.stereotype.Component;
+
+import com.upayment.camel.RestRouteBuilder;
+import com.upayment.camel.util.ConstantesCamel;
+import com.upayments.pg.caja.api.io.soap.getcardpinoffset.DataInput;
+
+@Component(value="getCardPinOffsetByPan")
+public class GetCardPinOffsetByPanRoute extends RestRouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		
+	    rest("/GetCardPinOffsetByPan")
+	    	.post("/")
+	    	.description("Servicio para invocar a WS SOAP GetCardPinOffsetByPan")
+	    	.type(DataInput.class)
+	    	.outType(String.class)
+	    	.consumes("application/json")
+	    	.produces("text/plain")
+	    	.to("direct:GetCardPinOffsetByPan");	
+	    
+	    from("direct:GetCardPinOffsetByPan")
+	    	.handleFault()
+		    .log("${body}")
+		    .setProperty("completeRequest",simple("${body}"))
+		    .log("(!) post guardado completeRequest")
+		    .unmarshal().json(JsonLibrary.Jackson, DataInput.class)
+		    .log("(!) post serializado")
+		    .setBody(simple("${body.header}"))
+		    .log("(!) pre envio a procesa header")
+		    .process("commonHeaderPrepare")
+		    .log("(!) post envio a procesa header")		
+		    .setBody(simple("${property.completeRequest}"))
+		    .log("BODY PRE VM -> ${body}")
+		    .unmarshal().json(JsonLibrary.Jackson, DataInput.class)
+		    .to("velocity:GetCardPINOffsetByPan_Req.vm")
+		    .log("HEADER IDA -> ${in.header.CamelSpringWebServiceSoapHeader}")
+		    .log("BODY IDA -> ${body}")
+		    .log("DESTINO: -> {{soap.remote.GetCardPINOffsetByPan.ep}} ACCION: -> {{soap.remote.GetCardPINOffsetByPan.action}} ")
+	    	.to("spring-ws:{{soap.remote.GetCardPINOffsetByPan.ep}}?soapAction={{soap.remote.GetCardPINOffsetByPan.action}}")
+	    	.log("HTTP RESPONSE DE VUELTA -> ${header.CamelHttpResponseCode} ")
+	    	.log("BODY VUELTA -> ${body}")  
+	    	
+	    	.convertBodyTo(String.class) //Para pasar XML por convertidor XSLT, debe entrar como String
+	    	.setHeader("retCode", constant("ASDF"))	
+	    	.setHeader("retMsg", constant("ASDF"))
+	    	.to("xslt:GetCardPINOffsetByPan_Body_Rsp.xsl") //Por defecto, output es string
+	    	.log("BODY VUELTA POST XSLT-> ${body}")  	
+	    	
+	    	.convertBodyTo(String.class)
+	    	.to("xslt:GetCardPINOffsetByPan_Aux_Rsp.xsl")
+	    	.log("BODY VUELTA POST XSLT 2-> ${body}")  	
+	    	
+	    	.convertBodyTo(String.class)
+	    	.to("xslt:Remove-Namespaces.xsl")
+	    	.log("BODY VUELTA POST XSLT 3-> ${body}")  	
+	    	
+	    	.marshal().xmljson(super.getXmlJsonOptions())
+	    	.log("BODY RECONVERTIDO -> ${body}")
+
+	        .setHeader(ConstantesCamel.HTTP_RESPONSE_CODE, simple(ConstantesCamel.HTTP_200));		   	    
+		
+	}
+
+}
---
+++ u-pay-pg-ws-api-procesador/src/main/resources/GetCardPINOffsetByPan_Body_Rsp.xsl
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" 
+xmlns:xsl="http://www.w3.org/1999/XSL/Transform" 
+xmlns:resp="http://xsd.nexuschile.cl/Prepaid/GetCardPINOffsetByPan/Resp-v2017.10">
+<xsl:output method="xml" />
+<xsl:template match="//resp:Data">
+<Data>
+    <RetCode><xsl:value-of select="resp:RetCode" /></RetCode>
+    <RetMsg><xsl:value-of select="resp:RetMsg" /></RetMsg>
+    <CardOffset><xsl:value-of select="resp:CardOffset" /></CardOffset>
+</Data>
+</xsl:template>
+</xsl:stylesheet>
