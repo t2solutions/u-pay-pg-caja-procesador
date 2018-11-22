f5fb3c0aaa3964d288587fb30bd3319dbe2228d3
Tomas Thoms Huerta <tthoms@gmail.com>
2018-11-22 17:26:52 -0300

    POJO EditClient faltaba una propiedad

--- upay-pg-caja-entities/src/main/java/com/upayments/pg/caja/api/io/soap/editclient/DataInput.java
+++ upay-pg-caja-entities/src/main/java/com/upayments/pg/caja/api/io/soap/editclient/DataInput.java
@@ -8,12 +8,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.upayments.pg.caja.api.io.soap.HeaderRequest;
 
-@JsonPropertyOrder({ "Header", "Data" })
+@JsonPropertyOrder({ "Header", "EditClientReq" })
 public class DataInput implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	private HeaderRequest header;
+	private EditClientReq editClientReq;
 
 	public DataInput() {
 		super();
@@ -25,6 +26,15 @@ public class DataInput implements Serializable {
 	}
 	public void setHeader(HeaderRequest header) {
 		this.header = header;
+	}
+	
+	@JsonProperty("EditClientReq")
+	public EditClientReq getEditClientReq() {
+		return editClientReq;
+	}
+	
+	public void setEditClientReq(EditClientReq editClientReq) {
+		this.editClientReq = editClientReq;
 	}
 
 
