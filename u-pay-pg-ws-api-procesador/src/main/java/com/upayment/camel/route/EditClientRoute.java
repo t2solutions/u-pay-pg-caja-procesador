5f313346a541a67bc1eacd1cd9681d18d52ae96f
Tomas Thoms Huerta <tthoms@gmail.com>
2018-11-23 01:59:36 -0300

    Servicio Modificacion Cliente

---
+++ u-pay-pg-ws-api-procesador/src/main/java/com/upayment/camel/route/EditClientRoute.java
@@ -0,0 +1,68 @@
+package com.upayment.camel.route;
+
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.stereotype.Component;
+
+import com.upayment.camel.RestRouteBuilder;
+import com.upayment.camel.util.ConstantesCamel;
+import com.upayments.pg.caja.api.io.soap.editclient.DataInput;
+
+@Component(value="editClient")
+public class EditClientRoute extends RestRouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		
+	    rest("/editClient")
+	    	.post("/")
+	    	.description("Servicio para invocar a WS SOAP EditClient")
+	    	.type(DataInput.class)
+	    	.outType(String.class)
+	    	.consumes("application/json")
+	    	.produces("text/plain")
+	    	.to("direct:EditClient");	
+	    
+	    from("direct:EditClient")
+	    	.handleFault()
+		    .log("${body}")
+		    .setProperty("completeRequest",simple("${body}"))
+		    .log("(!) post guardado completeRequest")
+		    .unmarshal().json(JsonLibrary.Jackson, DataInput.class)
+		    .log("(!) post serializado")
+		    .setBody(simple("${body.header}"))
+		    .log("(!) pre envio a procesa header")
+		    .process("commonHeaderPrepare")
+		    .log("(!) post envio a procesa header")		
+		    .setBody(simple("${property.completeRequest}"))
+		    .log("BODY PRE VM -> ${body}")
+		    .unmarshal().json(JsonLibrary.Jackson, DataInput.class)
+		    .to("velocity:EditClient_Req.vm")
+		    .log("HEADER IDA -> ${in.header.CamelSpringWebServiceSoapHeader}")
+		    .log("BODY IDA -> ${body}")
+		    .log("DESTINO: -> {{soap.remote.EditClient.ep}} ACCION: -> {{soap.remote.EditClient.action}} ")
+	    	.to("spring-ws:{{soap.remote.EditClient.ep}}?soapAction={{soap.remote.EditClient.action}}")
+	    	.log("HTTP RESPONSE DE VUELTA -> ${header.CamelHttpResponseCode} ")
+	    	.log("BODY VUELTA -> ${body}")  
+	    	
+	    	.convertBodyTo(String.class) //Para pasar XML por convertidor XSLT, debe entrar como String
+	    	.setHeader("retCode", constant(""))	
+	    	.setHeader("retMsg", constant(""))
+	    	.to("xslt:EditClient_Body_Rsp.xsl") //Por defecto, output es string
+	    	.log("BODY VUELTA POST XSLT-> ${body}")  	
+	    	
+	    	.convertBodyTo(String.class)
+	    	.to("xslt:EditClient_Aux_Rsp.xsl")
+	    	.log("BODY VUELTA POST XSLT 2-> ${body}")  	
+	    	
+	    	.convertBodyTo(String.class)
+	    	.to("xslt:Remove-Namespaces.xsl")
+	    	.log("BODY VUELTA POST XSLT 3-> ${body}")  	
+	    	
+	    	.marshal().xmljson(super.getXmlJsonOptions())
+	    	.log("BODY RECONVERTIDO -> ${body}")
+
+	        .setHeader(ConstantesCamel.HTTP_RESPONSE_CODE, simple(ConstantesCamel.HTTP_200));		   	    
+		
+	}
+
+}
