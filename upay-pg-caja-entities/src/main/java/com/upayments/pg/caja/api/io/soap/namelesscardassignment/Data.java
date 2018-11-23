d418084cc3ca76f15c81920427b0591a04aee192
Tomas Thoms Huerta <tthoms@gmail.com>
2018-11-23 17:17:19 -0300

    POJOS Servicio asignacion tarjeta innominada

---
+++ upay-pg-caja-entities/src/main/java/com/upayments/pg/caja/api/io/soap/namelesscardassignment/Data.java
@@ -0,0 +1,105 @@
+package com.upayments.pg.caja.api.io.soap.namelesscardassignment;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({ "BankCode", "TransactionID", "UniqueRefNumber",  "MsgCode", "RRN", "PayerContractScope", "PayerContractIdentifier", "PayerContractRelation",
+	"PayerCardSequenceNumber", "PayerExpirationDate","PayeeContractScope", "PayeeContractIdentifier", "PayeeContractRelation", "PayeeCardSequenceNumber",
+	"PayeeExpirationDate", "Amount", "Currency", "TransactionAttributes", "TransactionDetails", "TransactionDate", "OperatorID", "PostingDate",
+	"UserInfo" })
+public class Data implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String bankCode;
+	private String reason;
+	private String cardNumber;
+	private String identityCardNumber;
+	private String contractNumber;
+	private String embossedFirstName;
+	private String embossedLastName;
+	
+	private String userInfo;	
+	
+	public Data() {
+		super();
+	}
+	
+	@JsonProperty("BankCode")
+	public String getBankCode() {
+		return bankCode;
+	}
+
+	public void setBankCode(String bankCode) {
+		this.bankCode = bankCode;
+	}
+
+	@JsonProperty("Reason")
+	@JsonInclude(Include.NON_DEFAULT)
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	@JsonProperty("CardNumber")
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	@JsonProperty("IdentityCardNumber")
+	public String getIdentityCardNumber() {
+		return identityCardNumber;
+	}
+
+	public void setIdentityCardNumber(String identityCardNumber) {
+		this.identityCardNumber = identityCardNumber;
+	}
+
+	@JsonProperty("IdentityCardNumber")
+	public String getContractNumber() {
+		return contractNumber;
+	}
+
+	public void setContractNumber(String contractNumber) {
+		this.contractNumber = contractNumber;
+	}
+
+	@JsonProperty("EmbossedFirstName")
+	public String getEmbossedFirstName() {
+		return embossedFirstName;
+	}
+
+	public void setEmbossedFirstName(String embossedFirstName) {
+		this.embossedFirstName = embossedFirstName;
+	}
+
+	@JsonProperty("EmbossedLastName")
+	public String getEmbossedLastName() {
+		return embossedLastName;
+	}
+
+	public void setEmbossedLastName(String embossedLastName) {
+		this.embossedLastName = embossedLastName;
+	}
+
+	@JsonProperty("UserInfo")
+	public String getUserInfo() {
+		return userInfo;
+	}
+
+	public void setUserInfo(String userInfo) {
+		this.userInfo = userInfo;
+	}
+
+}
