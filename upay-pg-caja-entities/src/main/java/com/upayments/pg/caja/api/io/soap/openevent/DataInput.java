162cc577f9d24482a937be880f228a179e3b4743
Tomas Thoms Huerta <tthoms@gmail.com>
2018-12-03 11:39:42 -0300

    Corrección problema de falta de Header con OpenEvent

--- upay-pg-caja-entities/src/main/java/com/upayments/pg/caja/api/io/soap/openevent/DataInput.java
+++ upay-pg-caja-entities/src/main/java/com/upayments/pg/caja/api/io/soap/openevent/DataInput.java
@@ -5,17 +5,27 @@ import java.io.Serializable;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.upayments.pg.caja.api.io.soap.EmptyHeaderRequest;
 
-@JsonPropertyOrder({ "DATA" })
+@JsonPropertyOrder({ "Header", "DATA" })
 public class DataInput implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	private EmptyHeaderRequest header;
 	private Data data;
 
 	public DataInput() {
 		super();
 	}
+	
+	@JsonProperty("Header")
+	public EmptyHeaderRequest getHeader() {
+		return header;
+	}
+	public void setHeader(EmptyHeaderRequest header) {
+		this.header = header;
+	}	
 	
 	@JsonProperty("DATA")
 	public Data getData() {
