048dc8d7ce526b4bb155e7e3fd7e801842754ecb
Tomas Thoms Huerta <tthoms@gmail.com>
2019-01-22 17:06:57 -0300

    Cambio en la manera en que se arma el header

--- u-pay-pg-ws-api-procesador/src/main/java/com/upayment/camel/processor/CommonHeaderInvokerProcessor.java
+++ u-pay-pg-ws-api-procesador/src/main/java/com/upayment/camel/processor/CommonHeaderInvokerProcessor.java
@@ -21,15 +21,24 @@ public class CommonHeaderInvokerProcessor implements Processor {
 		//Recibir la ristra JSON serializada del objeto Common de cabecera
 		HeaderRequest rqst = exchange.getIn().getBody(HeaderRequest.class);
 		//Cargar header generado con los datos recibidos
-		exchange.getIn().setHeader(SpringWebserviceConstants.SPRING_WS_SOAP_HEADER, createCommonHeader(rqst));
-
+		
+		exchange.setProperty("hdr", obtainCommonHeader(rqst));
+		exchange.getIn().setHeader(SpringWebserviceConstants.SPRING_WS_SOAP_HEADER, obtainCommonHeader(rqst));
+		
 		System.out.println("FIN PRE-PROCESADOR DE HEADER");
 		
 	}
 	
 	private Map<String, Object> createCommonHeader(HeaderRequest gottenRequest) {
 		StringBuffer sbHeader = new StringBuffer();
-        Map<String, Object> headers = new HashMap<>();
+        Map<String, Object> headers = prepareCommonHeader(gottenRequest,
+				sbHeader);
+        headers.put("CamelSpringWebserviceSoapHeader", sbHeader.toString());
+        return headers;
+	}
+
+	private String obtainCommonHeader(HeaderRequest gottenRequest) {
+		StringBuilder sbHeader = new StringBuilder();
         sbHeader.append("<ns:HeaderRequest xmlns:ns=\"http://xsd.nexuschile.cl/Credit/Common/Enterprise/HeaderRequest/201707\">");
         sbHeader.append("<ns:Common>");
 	        sbHeader.append("<ns:ServiceID>");
@@ -46,9 +55,30 @@ public class CommonHeaderInvokerProcessor implements Processor {
 	        sbHeader.append("</ns:RequestDate>");        
 	     sbHeader.append("</ns:Common>");
 	     sbHeader.append("</ns:HeaderRequest>");
-        headers.put("CamelSpringWebserviceSoapHeader", sbHeader.toString());
-        return headers;
-}
+	     return sbHeader.toString();
+	}	
+	
+	private Map<String, Object> prepareCommonHeader(
+			HeaderRequest gottenRequest, StringBuffer sbHeader) {
+		Map<String, Object> headers = new HashMap<>();
+        sbHeader.append("<ns:HeaderRequest xmlns:ns=\"http://xsd.nexuschile.cl/Credit/Common/Enterprise/HeaderRequest/201707\">");
+        sbHeader.append("<ns:Common>");
+	        sbHeader.append("<ns:ServiceID>");
+	        sbHeader.append(gottenRequest.getHeaderRequestCommon().getCommon().getServiceID());
+	        sbHeader.append("</ns:ServiceID>");
+	        sbHeader.append("<ns:ServiceVersion>");
+	        sbHeader.append(gottenRequest.getHeaderRequestCommon().getCommon().getServiceVersion());
+	        sbHeader.append("</ns:ServiceVersion>");	        
+	        sbHeader.append("<ns:Product>");
+	        sbHeader.append(gottenRequest.getHeaderRequestCommon().getCommon().getProduct());
+	        sbHeader.append("</ns:Product>");	        
+	        sbHeader.append("<ns:RequestDate>");
+	        sbHeader.append(gottenRequest.getHeaderRequestCommon().getCommon().getRequestDate());
+	        sbHeader.append("</ns:RequestDate>");        
+	     sbHeader.append("</ns:Common>");
+	     sbHeader.append("</ns:HeaderRequest>");
+		return headers;
+	}
 	
 
 }
